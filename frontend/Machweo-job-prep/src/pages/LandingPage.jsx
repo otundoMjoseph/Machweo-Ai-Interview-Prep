3174abb27b7b691fd36801d40c278976e9d71dc6
otundoMjoseph <otundomainjoseph@gmail.com>
2025-11-03 15:36:34 +0300

    patched the frontend

--- frontend/Machweo-job-prep/src/pages/LandingPage.jsx
+++ frontend/Machweo-job-prep/src/pages/LandingPage.jsx
@@ -3,12 +3,89 @@ import HERO_IMG from '../assets/Machweohero.jpeg'
 import {APP_FEATURES} from '../utils/data'
 import { useNavigate } from 'react-router-dom'
 
+
 const LandingPage = () => {
-  const navigate = useNavigate()
+const navigate = useNavigate()
+
+
   const [openAuthModel, setOpenAuthModel] = useState(false)
-  const[currentPage, setCurrentPage] = useState("login")
+  const [currentPage, setCurrentPage] = useState("login")
+  
+  const handleCTA = ()=>{}
   return (
-    <div>LandingPage</div>
+    <>
+    <div className='w-full min-h-full bg-[#FFFCEF]'>
+      <div className='w-[500px] h-[500px] bg-amber-200/20 blur-[65px] absolute top-0 left' >
+
+      </div>
+      <div className='container mx-auto px-4 pt-6 pb-[200px relative z-10]'>
+        {/*Header  */}
+        <header className='flex justify-between items-center mb-16'>
+          <div className="text-xl text-black font-bold ">
+            Interview Prep AI
+          </div>
+
+          <button className='bg-linear-to-r from-[#FF9324] to-[#c99afb] text-sm semibold text-white px-7 py-2.5 rounded-full hover:bg-black hover:text-white border border-white transition-colors cursor-pointer '
+            onClick={() => (setOpenAuthModel(false))}>
+            Login/Signup
+          </button>
+        </header>
+        {/*Hero Content */}
+
+        <div className="flex flex-col md:flex-row items-center">
+          <div className="w-full md:w-1/2 pr-4 mb-8 md:mb-0">
+        <div className="flex-items center jusify-left mb-2">
+              <div className="flex-items center gap-2 text[13px] text-amber-600 font-semibold bg-amber-100 px-3 py-1 rounded-full border border-amber-300">
+                  {/*to import these icon */}
+                  
+                    AI Powered
+              </div>
+            </div>
+            <h1 className="text-5xl text-black font-medium mb-6 leading-tight">
+              <span className="text-transaparent bg-clip-text bg-[radial-gradient(circle,_#FF9324_0%, #FCD760_100% )] bg-[length:200%_200%] animate-text-shine font-semibold  ">
+                Ace Interviews with <br />
+                Machweo AI Interview Prep
+              </span>
+              Learning
+            </h1>
+          </div>
+          
+          <div className="w-full md:w-1/2">
+            <p className="text-[17px] text-gray-900 mr-0 md:mr-20 mb-6">
+              Get-role specific questions, expand answers when you need them,
+              Dive deeper into concepts, and organize everything your way, 
+              from preparations to mastery -your ultimate interview toolkit is here.
+
+            </p>
+            
+            <button className="bg-black text-sm font-semibold text-white px-7 py-2.5 rounded-full hover:yellow-100 hover:text-black border border-yellow-50 hover:border-yellow-300 transition-colors cursor-pointer"
+            onClick={handleCTA}
+            
+            >
+              Get Started 
+              
+          </button>
+
+
+          </div>
+          </div>
+          </div>
+      </div>
+
+      <div className="">
+        <div className="">
+
+          <section className="">
+
+        <img src={HERO_IMG} alt="Hero Image" className=''/>
+      </section>
+        </div>
+
+      </div>
+
+      
+      </>
+    
   )
 }
 
